52f011082525063a14f309bbc23f71474ecb0e60
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-10-23 10:34:39 -0300

    just add your PUBLIC KEY and run the server anywhere

--- index.js
+++ index.js
@@ -6,29 +6,40 @@ const puppeteer = require("puppeteer");
 
 const my_address = "0x6599cA2767Fa78bE271ef85557E755C6687Ee3Ca";
 
-setInterval(() => {
-  console.log("calling from server");
-  (async () => {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1000, height: 1700 });
-    await page.goto("https://faucet.fantom.network/");
+// run the bot first then run it each 10 minutes
+const run = async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1000, height: 1700 });
+  await page.goto("https://faucet.fantom.network/");
 
-    // await page.waitForNavigation();
-    await page.waitForTimeout(5000);
-    await page.type("input", my_address);
-    await page.waitForTimeout(1000);
-    await page.click('button[class="App-request-btn"]');
-    // await page.waitForTimeout(8000);
-    await page.waitForNavigation();
-    // await page.waitForNavigation();
-    // await page.waitForTimeout(1000 * 600);
+  // await page.waitForNavigation();
+  await page.waitForTimeout(5000);
+  await page.type("input", my_address);
+  await page.waitForTimeout(1000);
+  await page.click('button[class="App-request-btn"]');
+  // await page.waitForTimeout(8000);
+  await page.waitForNavigation();
+  // await page.waitForNavigation();
+  // await page.waitForTimeout(1000 * 600);
 
-    await browser.close();
-  })();
-  // }, 1000 * 10);
-}, 1100 * 60 * 10);
+  await browser.close();
+};
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
+run();
+
+setInterval(() => {
+  (async () => {
+    run();
+  })();
+}, 1100 * 60 * 13);
+
+// https://kovan.chain.link/
+// https://ropsten.chain.link/
+// https://faucets.chain.link/
+// // document.querySelector('li[class="Box-sc-1vpmd2a-0 cDTLBZ"]').parentNode.children
+// https://faucet.polygon.technology/
